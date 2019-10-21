07d463ca4e57cafa3662ef41b748167dab5a2fad
Serge Cornet <serge_cornet@wifi-student7-829.sri.ucl.ac.be>
2019-10-21 22:08:49 +0200

    Ajout de la fonction en queue

--- src/ordered_ll.c
+++ src/ordered_ll.c
@@ -19,15 +19,71 @@ ordered_ll * create_ordered_ll(){
     return q;
 }
 
-int enqueue(ordered_ll *q, pkt_t *pkt){
-    if(q==NULL){return -1;}
-    if(pkt==NULL){return -2;}
-    node *new_node = create_node();
-    new_node->pkt = pkt;
-    new_node->next = q->front;
-    q->front = new_node;
-    free(new_node);
-    return 0;
+void enqueue(ordered_ll *q, pkt_t *pkt){
+    int type_pkt = pkt_get_type(pkt);
+    if (type_pkt == 3){
+        node * curseur_node = q.front;
+        node * new_node = create_node();
+        if (new_node == NULL){
+            new_node->pkt = pkt;
+            new_node->next = NULL;
+            q->front = new_node;
+            return;
+        }
+        else{
+            if (curseur_node->next == NULL){
+                new_node->pkt = pkt;
+                new_node->next = NULL;
+                q->front->next = new_node;
+                return;
+            }
+            else{
+                while(curseur_node->next != NULL){
+                    curseur_node = curseur_node->next;
+            }
+            new_node->pkt = pkt;
+            new_node->next = NULL;
+            curseur_node->next = new_node;
+            return;
+            }
+        }
+    }
+    else if(type_pkt == 2){
+        if(q==NULL){return;}
+        if(pkt==NULL){return;}
+        if (q->front == NULL){
+            node *new_node = create_node();
+            new_node->pkt = pkt;
+            new_node->next = NULL;
+            q->front = new_node;
+            return;
+        }
+        int type = pkt_get_type(q->front->pkt);
+        if (type == 3){
+            node *new_node = create_node();
+            new_node->pkt = pkt;
+            new_node->next = q->front;
+            q->front = new_node;
+            return;
+        }
+        int new_seqnum = pkt_get_seqnum(pkt);
+        int actual_seqnum = pkt_get_seqnum(q->front->pkt);
+        if (new_seqnum > actual_seqnum){
+            node *new_node = create_node();
+            new_node->pkt = pkt;
+            new_node->next = q->front->next;
+            delete_node(q->front);
+            q->front = new_node;
+            return;
+    }
+    else{
+        fprintf(stderr,"The packet should be of type Nack or Ack\n");
+    }
+}
+
+void delete_node(node *node){
+    pkt_del(node->pkt);
+    free(node);
 }
 int add(ordered_ll * q, pkt_t *pkt){
     if(q==NULL){return -1;}
@@ -73,14 +129,14 @@ pkt_t * retrieve(ordered_ll * q){
     node *n = create_node();
     n = q->front;
     if (q->size == 1){
-        free(n);
+        delete_node(n);
         q->front = NULL;
         q->size = q->size-1;
         return new_pkt;
     }
     q->front = n->next;
     q->size = q->size - 1;
-    free(n);
+    delete_node(n);
     return new_pkt;
 }
 
@@ -106,80 +162,5 @@ int printQ(ordered_ll *q){
         retrieve(q);
         //printf("%d\n",(int)pkt_get_type(pek));
     }
-    return 0;
-}
-int main() {
-    ordered_ll* q = create_ordered_ll();
-    //Node *n=malloc(sizeof(struct Node));
-
-    pkt_t * pkt = pkt_new();
-	size_t * len = (size_t*) malloc(sizeof(size_t));
-	*len = 30;
-	char * buf = (char *) malloc(30);
-	buf[0] = (char) 0x5c;
-	buf[1] = (char) 0x0b;
-	buf[2] = (char) 0x7b;
-	buf[3] = (char) 0x17;
-	buf[4] = (char) 0x00;
-	buf[5] = (char) 0x00;
-	buf[6] = (char) 0x00;
-	buf[7] = (char) 0x50;
-	buf[8] = (char) 0x12;
-	buf[9] = (char) 0x12;
-	buf[10] = (char) 0x86;
-	buf[11] = (char) 0x68;
-	buf[12] = (char) 0x65;
-	buf[13] = (char) 0x6c;
-	buf[14] = (char) 0x6c;
-	buf[15] = (char) 0x6f;
-	buf[16] = (char) 0x20;
-	buf[17] = (char) 0x77;
-	buf[18] = (char) 0x6f;
-	buf[19] = (char) 0x72;
-	buf[20] = (char) 0x6c;
-	buf[21] = (char) 0x64;
-	buf[22] = (char) 0x0d;
-	buf[23] = (char) 0x4a;
-	buf[24] = (char) 0x11;
-	buf[25] = (char) 0x85;
-
-	pkt_status_code err = pkt_decode(buf, 26, pkt);
-	if( err == E_TYPE) printf("type \n");
-	if( err == E_LENGTH) printf("length \n");
-	if( err == E_CRC) printf("crc \n");
-	if( err == E_WINDOW) printf("window \n");
-	if( err == E_SEQNUM) printf("seqnum\n");
-	if( err == E_NOMEM) printf("nomem\n");
-	if( err == E_NOHEADER) printf("header\n");
-	if( err == PKT_OK) printf("okok \n");
-	if( err == E_UNCONSISTENT) printf("unconsistent \n");
-
-    int test = add(q,pkt);
-    if(test!=0){
-        printf("err1or");
-    }
-
-    int test2=add(q,pkt);
-    if(test2!=0){
-        printf("error");
-    }
-
-    int test3=add(q,pkt);
-    if(test3!=0){
-        printf("error");
-    }
-
-    //printf("enqueued");
-
-    printQ(q);
-    printf("la taille apres deque %d\n", q->size);
-
-    /*pkt_t *pK = pkt_new();
-    if((int)pkt_set_type(pK, PTYPE_ACK)<0){return -1;}
-    enqueue(q, pK);
-    printQ(q);*/
-
-
-
     return 0;
 }
